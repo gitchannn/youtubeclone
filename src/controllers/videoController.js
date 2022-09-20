577c69e3fabcefe0fb16c952816b596f1d85a332
Eunkeee <eunkeeeee@gmail.com>
2022-09-20 16:45:50 +0900

    5.2 Partial 사용해서 footer 분리하기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,5 @@
 export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-  console.log(req.params);
-  res.send(`Watch video ${req.params.id}`);
-};
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
