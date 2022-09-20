1b31c024c8a0c5e3bf552486df3ea14c9a82556a
Eunkeee <eunkeeeee@gmail.com>
2022-09-20 13:04:46 +0900

    3.6 MiddleWare (global middleware, why)
    app.use: for global middleware (순서가 중요함)

--- src/server.js
+++ src/server.js
@@ -3,22 +3,32 @@ import express from "express";
 const PORT = 4000;
 
 const app = express();
-const gossipMiddleWare = (req, res, next) => {
-  console.log(`Someone is going to ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`logger: ${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleWare = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    console.log("Not Allowed.");
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue...");
   next();
 };
 const handleHome = (req, res) => {
   // finalWare: return으로 인해 연결 종료
-  console.log("I'm the finalWare!");
+  console.log("finalWare: I'm the finalWare!");
   return res.send("I still love you");
 };
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
 };
 
-app.use(gossipMiddleWare);
+app.use(logger);
+app.use(privateMiddleWare);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
