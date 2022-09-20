a01628c23ed311ae9fe11f70eb22b2eafed6264a
Eunkeee <eunkeeeee@gmail.com>
2022-09-20 23:50:52 +0900

    6.0 array database 다루기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,33 +1,39 @@
-export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
-  return res.render("home", { pageTitle: "Home", videos });
+let videos = [
+  // fake database
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos });
+
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1]; // video index : 0, 1, 2 VS id : 1, 2, 3
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,7 +4,7 @@ import { see, edit, deleteVideo, upload } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
