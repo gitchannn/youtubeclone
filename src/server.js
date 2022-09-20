b7ee830a345e015e6c083af6aa8420f08e38e214
Eunkeee <eunkeeeee@gmail.com>
2022-09-20 13:37:20 +0900

    3.11 Morgan (external middlewares)
    500줄 가까이 되는 내용을 1줄로 import 가능!!

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
+const logger = morgan("dev");
 
 const home = (req, res) => {
   return res.send("Home");
 };
+const login = (req, res) => {
+  return res.send("Login");
+};
 
+app.use(logger);
 app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
