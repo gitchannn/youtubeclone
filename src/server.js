efaa9c9ec127f83649112360ca6b1ae6c89dd9d9
Eunkeee <eunkeeeee@gmail.com>
2022-09-20 13:53:23 +0900

    4.1 Making Routers

--- src/server.js
+++ src/server.js
@@ -7,8 +7,16 @@ const logger = morgan("dev");
 app.use(logger);
 
 const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
 const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
 const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
