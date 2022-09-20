23233d37f4171465d671b729f8d3eccd90a585ef
Eunkeee <eunkeeeee@gmail.com>
2022-09-20 20:16:18 +0900

    5.8 pug iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,8 @@
-const fakeUser = {
-  username: "Nico",
-  loggedIn: true,
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  //   const videos = [];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
