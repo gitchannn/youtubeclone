fbe6958e3d87edf2d7e7417b67c0f2dd37fc9fcd
Eunkeee <eunkeeeee@gmail.com>
2022-09-20 16:01:48 +0900

    4.8 url parameters :{param이름}(정규식)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,8 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch");
+export const see = (req, res) => {
+  console.log(req.params);
+  res.send(`Watch video ${req.params.id}`);
+};
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,9 @@ import { see, edit, deleteVideo, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
 videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
