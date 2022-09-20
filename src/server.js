7b1b3da90ba00671be9e2cdc030a125f7c965fa2
Eunkeee <eunkeeeee@gmail.com>
2022-09-20 12:56:58 +0900

    3.5 MiddleWare

--- src/server.js
+++ src/server.js
@@ -3,14 +3,20 @@ import express from "express";
 const PORT = 4000;
 
 const app = express();
-
+const gossipMiddleWare = (req, res, next) => {
+  console.log(`Someone is going to ${req.url}`);
+  next();
+};
 const handleHome = (req, res) => {
+  // finalWare: return으로 인해 연결 종료
+  console.log("I'm the finalWare!");
   return res.send("I still love you");
 };
 const handleLogin = (req, res) => {
   return res.send("Login here.");
 };
 
+app.use(gossipMiddleWare);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 
