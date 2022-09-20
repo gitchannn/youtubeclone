5ce3e241a999992875037f69165307a02296fb87
Eunkeee <eunkeeeee@gmail.com>
2022-09-20 10:45:01 +0900

    3.2 GET Request
    Request = Browser: i want to get this URL.(GET Request) / server: ok

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 
-const app = express();
+// create application
 const PORT = 4000;
+const app = express();
 
+// configure application
+app.get("/", () => console.log("Somebody is trying to go home"));
+
+// open the application to the outside world
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
