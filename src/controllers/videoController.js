d000e67155a6577e39fc4af21724f63b89a84e38
Eunkeee <eunkeeeee@gmail.com>
2022-09-21 13:26:30 +0900

    6.5 upload video Route정리 + form 만들기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -32,7 +32,7 @@ export const trending = (req, res) =>
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1]; // video index : 0, 1, 2 VS id : 1, 2, 3
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
@@ -44,4 +44,11 @@ export const postEdit = (req, res) => {
   const { title } = req.body;
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  // here we will add a video to the videos array.
+  return res.redirect("/");
 };
--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use(morgan("dev")); // middleware
+// express application이 form의 value들을 이해할 수 있게 하는 middleware
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/videos", videoRouter);
