6ce33db42473b1a499ced3011341edfa42c508de
Eunkeee <eunkeeeee@gmail.com>
2022-09-22 09:17:50 +0900

    6.11 server, init (import, port)를 분리함

---
+++ src/init.js
@@ -0,0 +1,10 @@
+import "./db"; // connect to Mongo
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
