5df917784cdf03fda5e639f3858772099cf5bc0d
Eunkeee <eunkeeeee@gmail.com>
2022-09-22 09:21:43 +0900

    6.11 코드정리 (fake DB 지우기)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,43 +1,13 @@
-let videos = [
-  // fake database
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 3,
-  },
-];
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", videos });
-
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1]; // video index : 0, 1, 2 VS id : 1, 2, 3
-  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+  return res.render("watch", { pageTitle: "Watch" });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1]; // video index : 0, 1, 2 VS id : 1, 2, 3
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit");
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
@@ -51,14 +21,5 @@ export const getUpload = (req, res) => {
 export const postUpload = (req, res) => {
   // here we will add a video to the videos array.
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: 4,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { trending, search } from "../controllers/videoController";
+import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", trending); // video
+globalRouter.get("/", home); // video
 globalRouter.get("/join", join); // user
 globalRouter.get("/login", login); // user
 
