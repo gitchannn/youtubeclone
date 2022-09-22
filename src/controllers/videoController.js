d56f8ad78570fd21410808d5cfe316a91987dacc
Eunkeee <eunkeeeee@gmail.com>
2022-09-22 11:44:12 +0900

    6.16 코드정리

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -27,16 +27,20 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: Date.now(),
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
   console.log(dbVideo);
   return res.redirect("/");
 };
