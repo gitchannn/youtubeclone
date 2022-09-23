27feeb61fb6c47aafe750d35a561934d8f6f862f
Eunkeee <eunkeeeee@gmail.com>
2022-09-23 22:35:54 +0900

    7.5

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { getJoin, postJoin, login } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home); // video
 rootRouter.route("/join").get(getJoin).post(postJoin); // user
-rootRouter.get("/login", login); // user
+rootRouter.route("/login").get(getLogin).post(postLogin); // user
 rootRouter.get("/search", search); // video
 
 export default rootRouter;
