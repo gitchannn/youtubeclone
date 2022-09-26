a868d93ba16591f9a1e81bdea45c2501bdd1d4d8
Eunkeee <eunkeeeee@gmail.com>
2022-09-26 10:50:54 +0900

    7.8 Sessions, Cookies

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -9,9 +10,28 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
-app.use(morgan("dev")); // middleware
-// express application이 form의 value들을 이해할 수 있게 하는 middleware
+// middleware
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`);
+});
 
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
