b6053647778003206ebb6362b3b322119988a5dd
Eunkeee <eunkeeeee@gmail.com>
2022-09-27 14:02:16 +0900

    8.1 로그인여부에 따라서 url 못가게 막기 (all.(middleware))

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -6,12 +6,17 @@ import {
   postLogin,
 } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home); // video
-rootRouter.route("/join").get(getJoin).post(postJoin); // user
-rootRouter.route("/login").get(getLogin).post(postLogin); // user
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin); // user
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin); // user
 rootRouter.get("/search", search); // video
 
 export default rootRouter;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -9,14 +9,15 @@ import {
   getEdit,
   postEdit,
 } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/remove", remove);
-userRouter.get("/github/start", startGitHubLogin);
-userRouter.get("/github/finish", finishGitHubLogin);
+userRouter.get("/github/start", publicOnlyMiddleware, startGitHubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGitHubLogin);
 userRouter.get(":id", see);
 
 export default userRouter;
