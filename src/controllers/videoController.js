ea5a4ac8aff95274d8acefd51ad6197deffa3311
Eunkeee <eunkeeeee@gmail.com>
2022-09-28 09:49:30 +0900

    8.9 Video upload 성공!!!!

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -43,11 +43,13 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = async (req, res) => {
+  const { file: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       title,
       description,
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
