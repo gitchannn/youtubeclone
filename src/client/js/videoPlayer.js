cbd3edd1753601055fe9d6bd56166caf57fa1d06
Eunkeee <eunkeeeee@gmail.com>
2022-10-01 21:28:53 +0900

    11.1 Play, Pause Btn

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.querySelector("#play");
+const muteBtn = document.querySelector("#mute");
+const time = document.querySelector("#time");
+const volume = document.querySelector("#volume");
+
+const handlePlayClick = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+const handlePause = () => {
+  playBtn.innerText = "Play";
+};
+const handlePlay = () => {
+  playBtn.innerText = "Pause";
+};
+const handleMute = () => {};
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("play", handlePlay);
+video.addEventListener("pause", handlePause);
