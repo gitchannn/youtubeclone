5d1ebaae847f8e2906d4a64814befc08270b5f74
Eunkeee <eunkeeeee@gmail.com>
2022-10-03 13:45:03 +0900

    11.7 전체화면 버튼 만들기

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -5,6 +5,8 @@ const currentTime = document.querySelector("#currentTime");
 const totalTime = document.querySelector("#totalTime");
 const volumeRange = document.querySelector("#volume");
 const timeline = document.querySelector("#timeline");
+const FullScreenBtn = document.querySelector("#fullScreen");
+const videoContainer = document.querySelector("#videoContainer");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -65,9 +67,21 @@ const handleTimeLineChange = (event) => {
   video.currentTime = value;
 };
 
+const handleFullScreen = () => {
+  const fullscreen = document.fullscreenElement; // Boolean
+  if (fullscreen) {
+    FullScreenBtn.innerText = "Enter Full Screen";
+    document.exitFullscreen();
+  } else {
+    videoContainer.requestFullscreen();
+    FullScreenBtn.innerText = "Exit Full Screen";
+  }
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("input", handleTimeLineChange);
+FullScreenBtn.addEventListener("click", handleFullScreen);
