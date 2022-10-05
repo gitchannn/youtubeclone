9a468a2d93c54ac0677156b3cc59cbbeaa042766
Eunkeee <eunkeeeee@gmail.com>
2022-10-05 10:27:41 +0900

    13.2 비디오 녹화

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,40 @@
 const startBtn = document.querySelector("#startBtn");
 const video = document.querySelector("#preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log("recording done");
+    console.log("EVENT:", event);
+    console.log("EVENT DATA:", event.data);
+  };
+  console.log("RECORDER BEFORE:", recorder);
+  recorder.start();
+  console.log("RECORDER AFTER:", recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
+};
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
