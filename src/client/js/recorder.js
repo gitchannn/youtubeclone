f14ffe9f80ba9311ff216088d6069584eaf9f59d
Eunkeee <eunkeeeee@gmail.com>
2022-10-05 10:33:13 +0900

    13.3 녹화 후에 그자리에 비디오를 다시 보여줌

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,29 +2,28 @@ const startBtn = document.querySelector("#startBtn");
 const video = document.querySelector("#preview");
 
 let stream;
+let recorder;
 
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
 
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  const recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
-    console.log("recording done");
-    console.log("EVENT:", event);
-    console.log("EVENT DATA:", event.data);
+    const videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.play();
   };
-  console.log("RECORDER BEFORE:", recorder);
   recorder.start();
-  console.log("RECORDER AFTER:", recorder);
-  setTimeout(() => {
-    recorder.stop();
-  }, 5000);
 };
 const handleStop = () => {
   startBtn.innerText = "Start Recording";
   startBtn.removeEventListener("click", handleStop);
   startBtn.addEventListener("click", handleStart);
+
+  recorder.stop();
 };
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
