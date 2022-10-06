64b8088cdee41f7fa09aed8f746654ece083805f
Eunkeee <eunkeeeee@gmail.com>
2022-10-06 23:10:59 +0900

    16.7 실시간으로 보이기: pug에서 가져오지 말고, JS에서 새 element를 만들어 붙이자!

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,22 @@
 const videoContainer = document.querySelector("#videoContainer"); // data-id 퍼오려고 소환
 const form = document.querySelector("#commentForm");
 
+const addComment = (text) => {
+  // 실시간으로 보이기: pug에서 가져오지 말고, JS에서 새 element를 만들어 붙이자!
+  // 복잡하지 않다! 단지 HTML 구조를 베끼는 것 뿐
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = `${text}`;
+
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+
+  videoComments.appendChild(newComment);
+};
 const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
@@ -9,13 +25,16 @@ const handleSubmit = async (event) => {
   if (text === "" || text.trim() === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: { "Content-Type": "application/json" }, // middleware는 현재 우리가 보내는게 text라 생각함 => json이라고 말해줘야함
     body: JSON.stringify({ text }), // data를 여러 개 보내는 경우에 JSON으로 변환해야함
   });
+  const { status } = response;
+  if (status === 201) {
+    addComment(text);
+  }
   textarea.value = "";
-  window.location.reload();
 };
 
 if (form) {
