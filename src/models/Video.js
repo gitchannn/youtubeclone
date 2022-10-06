7163fe92cf6b4402d4a8729dc501c51c55ff9a1b
Eunkeee <eunkeeeee@gmail.com>
2022-10-06 16:49:33 +0900

    16.1 Comments 모델 만들기

--- src/models/Video.js
+++ src/models/Video.js
@@ -12,6 +12,7 @@ const videoSchema = new mongoose.Schema({
     views: { type: "Number", default: 0, required: true },
     rating: { type: "Number", default: 0, required: true },
   },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
