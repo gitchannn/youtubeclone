78aa94e85a35a52084ca945aceb70f9b1d02cd17
Eunkeee <eunkeeeee@gmail.com>
2022-10-06 22:55:18 +0900

    16.6 Realtime처럼 보이게 만드는법 : reload (매번 db에서 찾고 해서 부하걸림)

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,7 @@
 const videoContainer = document.querySelector("#videoContainer"); // data-id 퍼오려고 소환
 const form = document.querySelector("#commentForm");
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value; // 댓글 내용
@@ -9,12 +9,13 @@ const handleSubmit = (event) => {
   if (text === "" || text.trim() === "") {
     return;
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: { "Content-Type": "application/json" }, // middleware는 현재 우리가 보내는게 text라 생각함 => json이라고 말해줘야함
     body: JSON.stringify({ text }), // data를 여러 개 보내는 경우에 JSON으로 변환해야함
   });
   textarea.value = "";
+  window.location.reload();
 };
 
 if (form) {
