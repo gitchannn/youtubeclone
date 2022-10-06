b43971e5d881d1e5a30c2a6d6848dd3ab0eaa06d
Eunkeee <eunkeeeee@gmail.com>
2022-10-06 22:37:19 +0900

    16.6 video에서 comments를 populate하려면 video의 comment에 넣어줘야함!!!

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -14,7 +14,7 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
   console.log(video);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
@@ -142,5 +142,7 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id,
   });
+  video.comments.push(comment._id);
+  video.save();
   return res.sendStatus(201);
 };
