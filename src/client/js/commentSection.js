26173c116f7d77192f2495c14304b608bb363709
Eunkeee <eunkeeeee@gmail.com>
2022-10-07 16:54:21 +0900

    16.8 RealTime 댓글에도 id 붙이기

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,13 @@
 const videoContainer = document.querySelector("#videoContainer"); // data-id 퍼오려고 소환
 const form = document.querySelector("#commentForm");
 
-const addComment = (text) => {
+const addComment = (text, commentId) => {
   // 실시간으로 보이기: pug에서 가져오지 말고, JS에서 새 element를 만들어 붙이자!
   // 복잡하지 않다! 단지 HTML 구조를 베끼는 것 뿐
   const videoComments = document.querySelector(".video__comments ul");
   const newComment = document.createElement("li");
   newComment.className = "video__comment";
+  newComment.dataset.id = commentId;
   const icon = document.createElement("i");
   icon.className = "fas fa-comment";
   const span = document.createElement("span");
@@ -28,21 +29,17 @@ const handleSubmit = async (event) => {
   if (text === "" || text.trim() === "") {
     return;
   }
-  // 2. 프론트에서는 response가 오길 기다린 다음에
   const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: { "Content-Type": "application/json" }, // middleware는 현재 우리가 보내는게 text라 생각함 => json이라고 말해줘야함
     body: JSON.stringify({ text }), // data를 여러 개 보내는 경우에 JSON으로 변환해야함
   });
+  textarea.value = "";
   const { status } = response;
   if (status === 201) {
-    addComment(text);
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
   }
-  textarea.value = "";
-  // 3. response 안에서 json을 추출함
-  const json = await response.json();
-  console.log(json);
-  const { newCommentId } = json;
 };
 
 if (form) {
