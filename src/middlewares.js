31f88c77bba325a93fee2666c8a3a8eecfb64e6b
Eunkeee <eunkeeeee@gmail.com>
2022-10-10 16:23:28 +0900

    17.7 Server 대신 AWS S3에 저장해보자!

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,18 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "youtubeclonee",
+});
 
 export const localsMiddleWare = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -30,6 +44,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader,
 });
 
 export const videoUpload = multer({
@@ -37,4 +52,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader,
 });
