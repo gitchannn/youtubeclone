d1ad4cf7b380ea49739808fc89a4194c05988153
Eunkeee <eunkeeeee@gmail.com>
2022-10-11 15:14:26 +0900

    17.9 uploader middleware 나누고, 버켓 폴더 지정

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,9 +9,15 @@ const s3 = new aws.S3({
   },
 });
 
-const multerUploader = multerS3({
+const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "youtubeclonee",
+  bucket: "youtubeclonee/images",
+  acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "youtubeclonee/videos",
   acl: "public-read",
 });
 
@@ -45,7 +51,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: multerUploader,
+  storage: s3ImageUploader,
 });
 
 export const videoUpload = multer({
@@ -53,5 +59,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
-  storage: multerUploader,
+  storage: s3VideoUploader,
 });
