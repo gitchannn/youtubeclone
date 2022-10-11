58255542f1607e04e66344dfd916bcef414edf6f
Eunkeee <eunkeeeee@gmail.com>
2022-10-11 11:57:20 +0900

    14.5 코드 정리

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -7,47 +7,52 @@ let stream;
 let recorder;
 let videoFile;
 
+const files = {
+  input: "recordeing.webm",
+  output: "output.mp4",
+  thumb: "thumbnail.jpg",
+};
+
+const downloadFile = (fileUrl, fileName) => {
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+};
+
 const handleDownload = async () => {
   // 1. ffmpeg를 사용해 "유저의 서버"를 통해 로드함
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load();
   // 2. ffmpeg 세계에 파일을 만듦
-  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4"); // recording.webm을 넣으면 output.mp4가 나옴
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
+  await ffmpeg.run("-i", files.input, "-r", "60", files.output); // recording.webm을 넣으면 output.mp4가 나옴
   await ffmpeg.run(
     "-i",
-    "recording.webm",
+    files.input,
     "-ss",
     "00:00:01",
     "-frames:v",
     "1",
-    "thumbnail.jpg"
+    files.thumb
   );
 
-  const mp4File = ffmpeg.FS("readFile", "output.mp4");
-  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile", files.thumb);
 
-  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
-  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
+  const mp4Blob = new Blob([mp4File.buffer], { type: files.output });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: files.thumb });
 
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const a = document.createElement("a");
-  a.href = mp4Url;
-  a.download = "MyRecording.mp4";
-  document.body.appendChild(a);
-  a.click();
+  downloadFile(mp4Url, "MyRecording.mp4");
+  downloadFile(thumbUrl, "MyThumbnail.jpg");
 
-  const thumbA = document.createElement("a");
-  thumbA.href = thumbUrl;
-  thumbA.download = "MyThumbnail.jpg";
-  document.body.appendChild(thumbA);
-  thumbA.click();
-
-  ffmpeg.FS("unlink", "recording.webm");
-  ffmpeg.FS("unlink", "output.mp4");
-  ffmpeg.FS("unlink", "thumbnail.jpg");
+  ffmpeg.FS("unlink", files.input);
+  ffmpeg.FS("unlink", files.output);
+  ffmpeg.FS("unlink", files.thumb);
 
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
